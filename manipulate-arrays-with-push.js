abb68ef795e4a4bddb3294a3e547efac1df1051f
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:36 +0600

    Create manipulate-arrays-with-push.js

---
+++ manipulate-arrays-with-push.js
@@ -0,0 +1,8 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+// Matching with the required output on My VsCode:
+console.log("VsCode says: " + myArray); // Result Matched!!!
