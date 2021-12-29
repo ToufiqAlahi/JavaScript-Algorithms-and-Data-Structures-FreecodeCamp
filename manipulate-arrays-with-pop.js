5f5195c7913fe922464d2fed25738cedad1a2fa9
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:17 +0600

    Create manipulate-arrays-with-pop.js

---
+++ manipulate-arrays-with-pop.js
@@ -0,0 +1,8 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+let removedFromMyArray = myArray.pop();
+
+// Matching with the required output on My VsCode:
+console.log("VsCode says: " + "removedFromMyArray: " + removedFromMyArray + "     " + "myArray: " + myArray); // Result Matched!!!
