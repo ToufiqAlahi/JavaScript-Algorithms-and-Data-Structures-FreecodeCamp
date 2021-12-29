76b9d7d67e21b1221db2790a03494f510e6fe960
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:13 +0600

    Create local-scope-and-functions.js

---
+++ local-scope-and-functions.js
@@ -0,0 +1,16 @@
+function myLocalScope() {
+    // Only change code below this line
+    var myVar = ("Hello Toufiq!")
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+
+
+// Matching with the required output on My VsCode:
+
+// console.log(result); // Result Matched!!!
