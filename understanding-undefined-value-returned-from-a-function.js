74f01f465d09f4fbc44924c1e636b9820ef4a7f5
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:54 +0600

    Create understanding-undefined-value-returned-from-a-function.js

---
+++ understanding-undefined-value-returned-from-a-function.js
@@ -0,0 +1,22 @@
+// Setup
+let sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+    sum = sum+5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+
+
+// Matching with the required output on My VsCode:
+
+// console.log(result); // Result Matched!!!
