e1e49c607d712e9ba876e9910a7f0aca42cf724e
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:50:23 +0600

    Create assignment-with-a-returned-value.js

---
+++ assignment-with-a-returned-value.js
@@ -0,0 +1,12 @@
+// Setup
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
+console.log(processed);
+
+// Matching with the required output on My VsCode:
+// console.log(result); // Result Matched!!!
