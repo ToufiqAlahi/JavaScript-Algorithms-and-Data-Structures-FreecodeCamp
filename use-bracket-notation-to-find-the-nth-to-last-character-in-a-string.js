6d675f42c366c72cef8dea1114be6adb86c5f470
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:50:03 +0600

    Create use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js

---
+++ use-bracket-notation-to-find-the-nth-to-last-character-in-a-string.js
@@ -0,0 +1,8 @@
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+
+// Matching with the required output on My VsCode:
+console.log("VsCode says: " + secondToLastLetterOfLastName);
