a14c30e32ff132cb0a71b5f480073ae87c5e07ef
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:20 +0600

    Create manipulate-arrays-with-shift.js

---
+++ manipulate-arrays-with-shift.js
@@ -0,0 +1,13 @@
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+
+let removedFromMyArray = myArray.shift();
+
+
+
+// Matching with the required output on My VsCode:
+console.log("removedFromMyArray: " + removedFromMyArray + "       " + "myArray: " + myArray);
+
+// console.log("VsCode says: " + myArray); // Result Matched!!!
