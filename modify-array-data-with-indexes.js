295e6fe2a8c985b9334e091b445e68aa7b3b0868
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:24 +0600

    Create modify-array-data-with-indexes.js

---
+++ modify-array-data-with-indexes.js
@@ -0,0 +1,8 @@
+// Setup
+let myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
+
+// Matching with the required output on My VsCode:
+console.log("VsCode says: " + myArray); // Result Matched!!!
