5ab64b6ebb078e8aead385f6e78fcad3eec7e468
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:50:12 +0600

    Create shopping-list.js

---
+++ shopping-list.js
@@ -0,0 +1,6 @@
+const myList = [["subArray1", 0], ["subArray2", 1], ["subArray3", 2], ["subArray4", 3], ["subArray5", 4]];
+
+
+// Matching with the required output on My VsCode:
+console.log(myList);// Result Matched!!!
+
