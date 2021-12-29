e8145429307973cd7d3c3644a7230a64e7e4f766
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:33 +0600

    Create passing-values-to-functions-with-arguments.js

---
+++ passing-values-to-functions-with-arguments.js
@@ -0,0 +1,13 @@
+function functionWithArgs(x, y) {
+    console.log(x + y);
+
+}
+var result = functionWithArgs(7, 9);
+var result2 = functionWithArgs(1, 2);
+
+
+// Matching with the required output on My VsCode:
+
+console.log(result);
+// Result Matched!!!
+console.log(result2);
