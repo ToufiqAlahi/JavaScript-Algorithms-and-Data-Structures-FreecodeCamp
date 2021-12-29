b94d04103430404c32557a6e2a6c8af6bcf76eb5
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:42 +0600

    Create return-a-value-from-a-function-with-return.js

---
+++ return-a-value-from-a-function-with-return.js
@@ -0,0 +1,16 @@
+function timesFive(num) {
+    return num * 5;
+
+}
+var result = timesFive(5);
+var result2 = timesFive(2);
+var result3 =timesFive(0);
+
+
+// Matching with the required output on My VsCode:
+
+console.log(result);
+// Result Matched!!!
+console.log(result2);
+
+console.log(result3);
