95b1ae38c4a13ee3880ede7bd1cc654d7794b69d
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:08 +0600

    Create global-vs--local-scope-in-functions.js

---
+++ global-vs--local-scope-in-functions.js
@@ -0,0 +1,17 @@
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+    // Only change code below this line
+    const outerWear = "sweater";
+    // Only change code above this line
+    return outerWear;
+}
+
+myOutfit();
+
+
+
+// Matching with the required output on My VsCode:
+
+// console.log(result); // Result Matched!!!
