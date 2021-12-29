38f0f16ed2177e6aa101eacb3afdbd796501b685
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:49:27 +0600

    Create nest-one-array-within-another-array.js

---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [["My name is ", "Toufiq"], ["pi is equal to: ", 3.141592653]];
+
+// Matching with the required output on My VsCode:
+console.log("VsCode says: " + myArray); // Result Matched!!!
