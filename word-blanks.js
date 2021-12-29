f6a3e2317a2f9874e9507465eef65306e21f3206
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-29 23:50:02 +0600

    Create word-blanks.js

---
+++ word-blanks.js
@@ -0,0 +1,12 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " outside." ; // Change this line
+// Only change code above this line
+
+
+// Matching with the required output on My VsCode:
+console.log("VsCode says: " + wordBlanks); // Result Matched!!!
