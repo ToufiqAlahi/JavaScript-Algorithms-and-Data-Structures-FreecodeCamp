1dea29723cff425be2381d6c906165b1322196fc
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-30 09:59:47 +0600

    Create comparison-with-the-equality-operator (1).js

---
+++ comparison-with-the-equality-operator (1).js
@@ -0,0 +1,15 @@
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+
+
+// Matching with the required output on My VsCode:
+
+// console.log(result); // Result Matched!!!
