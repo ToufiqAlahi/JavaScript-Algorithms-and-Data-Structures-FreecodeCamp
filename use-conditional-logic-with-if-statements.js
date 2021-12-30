2a3d8e91b2965b368374083bdf41697aeed0123c
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-30 10:00:21 +0600

    Create use-conditional-logic-with-if-statements.js

---
+++ use-conditional-logic-with-if-statements.js
@@ -0,0 +1,14 @@
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if (wasThatTrue == true) {
+    return "Yes, that was true";
+  }
+  else {
+    return "No, that was false";
+  }
+
+}
+
+// Matching with the required output on My VsCode:
+
+// console.log(result); // Result Matched!!!
