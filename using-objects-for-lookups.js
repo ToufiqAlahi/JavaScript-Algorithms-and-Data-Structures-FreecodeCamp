2a72d09b90cdbd07312900b030a2be33710515bb
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-30 18:25:19 +0600

    Create using-objects-for-lookups.js

---
+++ using-objects-for-lookups.js
@@ -0,0 +1,16 @@
+function phoneticLookup(val) {
+  var result = "";
+
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  };
+  result = lookup[val];
+  return result;
+}
+
+
