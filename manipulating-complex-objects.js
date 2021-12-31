784ee3a3657b9ce2ac6178524d876bb450b50da4
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-31 10:21:29 +0600

    Create manipulating-complex-objects.js

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,27 @@
+const myMusic = [
+    {
+        "artist": "Freddie Mercury",
+        "title": "Bohemian Rhapsody",
+        "release_year": 1975,
+        "formats": [
+            "CD",
+            "8T",
+            "LP",
+        ],
+        "gold": true
+    },
+
+    {
+        "artist": "Freddie Mercury",
+        "title": "We Are the Champions",
+        "release_year": 1979,
+        "formats": [
+            "CD",
+            "8T",
+            "LP",
+        ],
+        "gold": true
+    },
+
+
+];
